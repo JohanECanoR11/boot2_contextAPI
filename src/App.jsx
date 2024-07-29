9566447a8d1528f97cca900cb668a6c97af99550
Johan E Cano Ruiz <johan.memincho@gmail.com>
2024-07-28 21:34:19 -0500

    Initial project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { AppRouter } from './routing/AppRouter'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <header className="App-header">
+        <h1>Ejercicio con Context API en React</h1>
+        <AppRouter />
+      </header>
+    </div>
+  )
+}
+
+export default App
